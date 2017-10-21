15a1d5fbf09da2e190184231b3851a29677dc07b
Mingle <451168805@qq.com>
2017-10-21 15:14:22 +0800

    version 6.0.0

--- js/showLocation.js
+++ js/showLocation.js
@@ -16,32 +16,121 @@ function locationFinder() {
     education.schools.forEach(function(school){
       locations.push(school.location);
     });
-
-    // iterates through work locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide:
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    work.jobs.forEach(function(job){
-      locations.push(job.location);
-    });
-
-    return locations;
-  }
-
+   }
 
  //创建地图
- var 
     var map = new AMap.Map('map', {
         zoom: 4
     });
 
+    AMapUI.load(['ui/misc/PathSimplifier', 'lib/$'], function(PathSimplifier, $) {
 
-//实例化标记点
- function addMarker() {
-        marker = new AMap.Marker({
-            icon: "http://webapi.amap.com/theme/v1.3/markers/n/mark_b.png",
-            position: [100,100]
+        if (!PathSimplifier.supportCanvas) {
+            alert('当前环境不支持 Canvas！');
+            return;
+        }
+
+        var pathSimplifierIns = new PathSimplifier({
+            zIndex: 100,
+            //autoSetFitView:false,
+            map: map, //所属的地图实例
+
+            getPath: function(pathData, pathIndex) {
+
+                return pathData.path;
+            },
+            getHoverTitle: function(pathData, pathIndex, pointIndex) {
+
+                if (pointIndex >= 0) {
+                    //point 
+                    return pathData.name + '，点：' + pointIndex + '/' + pathData.path.length;
+                }
+
+                return pathData.name + '，点数量' + pathData.path.length;
+            },
+            renderOptions: {
+
+                renderAllPointsIfNumberBelow: 100 //绘制路线节点，如不需要可设置为-1
+            }
         });
-        marker.setMap(map);
-    }
- addMarker();
+
+        window.pathSimplifierIns = pathSimplifierIns;
+
+        //设置数据
+        pathSimplifierIns.setData([{
+            name: '北京-玉林-柳州',
+            path: [
+                [116.41, 39.90],
+                [110.147395,22.63978],
+                [109.415823,24.313366]
+
+            ]
+        }]);
+
+        //对第一条线路（即索引 0）创建一个巡航器
+        var navg1 = pathSimplifierIns.createPathNavigator(0, {
+            loop: true, //循环播放
+            speed: 1000000 //巡航速度，单位千米/小时
+        });
+
+        navg1.start();
+    });
+    
+//引入SimpleMarker，loadUI的路径参数为模块名中 'ui/' 之后的部分
+AMapUI.loadUI(['overlay/SimpleMarker'], function(SimpleMarker) {
+    //启动页面
+    initPage(SimpleMarker);
+});
+
+function initPage(SimpleMarker) {
+
+    //创建SimpleMarker实例
+    new SimpleMarker({
+
+        //前景文字
+        iconLabel: '北京',
+
+        //图标主题
+        iconTheme: 'default',
+
+        //背景图标样式
+        iconStyle: 'red',
+
+        //...其他Marker选项...，不包括content
+        map: map,
+        position: [116.405285, 39.904989]
+    });
+
+    //创建SimpleMarker实例
+     new SimpleMarker({
+
+        //前景文字
+        iconLabel: '玉林',
+
+        //图标主题
+        iconTheme: 'default',
+
+        //背景图标样式
+        iconStyle: 'lightgreen',
+
+        //...其他Marker选项...，不包括content
+        map: map,
+        position: [110.147395,22.63978]
+    });
+    
+      new SimpleMarker({
+
+        //前景文字
+        iconLabel: '柳州',
+
+        //图标主题
+        iconTheme: 'default',
+
+        //背景图标样式
+        iconStyle: 'gray',
+
+        //...其他Marker选项...，不包括content
+        map: map,
+        position: [109.415823,24.313366]
+    });
+}
